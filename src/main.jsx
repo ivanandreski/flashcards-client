a3ab57f3a0886d7531700c6b89570f825a7f6465
ivan.andreski <ivan.andreski@gmail.com>
2023-04-03 11:24:36 +0200

    testing

--- src/main.jsx
+++ src/main.jsx
@@ -26,6 +26,7 @@ export const App = () => {
           <Route path={RouteNames.Register} element={<Register />} />
         </Route>
       </Routes>
+      <div>TESTING</div>
     </HashRouter>
   );
 };
