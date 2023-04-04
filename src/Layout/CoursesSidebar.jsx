bae87a58b1ce93ac4016a036f6f0d80f55a27441
ivan.andreski <ivan.andreski@gmail.com>
2023-04-04 21:02:13 +0200

    course basic breadcrumbs, sidebar border on sm

--- src/Layout/CoursesSidebar.jsx
+++ src/Layout/CoursesSidebar.jsx
@@ -6,7 +6,7 @@ const CoursesSidebar = () => {
   const { courses } = useFetchMyCourses();
 
   return (
-    <div className="w-full bg-gradient-to-l from-sky-500 to-fuchsia-800 p-1">
+    <div className="w-full bg-gradient-to-l from-sky-500 to-fuchsia-800 pl-1 pt-1 pb-1 xs:max-lg:pr-1">
       <div className="min-h-96 bg-white p-4">
         <h5 className="font-semibold text-4xl mb-2">My Courses</h5>
         <ul>
--- src/Pages/Course.jsx
+++ src/Pages/Course.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import useFetchCourseDecks from "../Hooks/Api/useFetchCourseDecks";
 import CourseDeck from "../Components/CourseDeck";
+import MyCoursesList from "../MockDB/MyCoursesList";
 
 const Course = () => {
   const { id } = useParams();
   const { decks } = useFetchCourseDecks();
 
   return (
-    <div className="p-4 grid gap-3 sm:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4">
-      {decks.map((deck) => {
-        return <CourseDeck key={deck.id} deck={deck} />;
-      })}
+    <div className="p-4">
+      <h1 className="text-4xl text-bold">Home/Courses/{MyCoursesList.Courses[id-1].name}</h1>
+      <div className="my-2 border border-black" />
+      <div className="grid gap-3 sm:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4">
+        {decks.map((deck) => {
+          return <CourseDeck key={deck.id} deck={deck} />;
+        })}
+      </div>
     </div>
   );
 };
